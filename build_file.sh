8d9e59046007251165ff179e678e106cb16bc9dc
KachhadiyaPrit <kachhadiyaprit89039@gmail.com>
2024-05-29 16:03:03 +0530

    new

--- build_file.sh
+++ build_file.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
 # Collect static files
-python3 -m manage.py collectstatic
-
-# Create a tarball of the media directory
-tar -czf media.tar.gz media
-
-# Upload the tarball to Vercel
-vercel deploy media.tar.gz
+python manage.py collectstatic

40641bf23e3d5231dcdf3b489a78cc9d71f65024
KachhadiyaPrit <kachhadiyaprit89039@gmail.com>
2024-05-29 15:43:17 +0530

    new

--- build_file.sh
+++ build_file.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+# Collect static files
 python3 -m manage.py collectstatic
+
+# Create a tarball of the staticfiles_build directory
+tar -czf staticfiles_build.tar.gz staticfiles_build
+
+# Upload the tarball to Vercel
+vercel deploy staticfiles_build.tar.gz

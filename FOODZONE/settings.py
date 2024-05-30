b2277b2cf470e157c7ebe62825c1226abc24a08f
KachhadiyaPrit <kachhadiyaprit89039@gmail.com>
2024-05-30 07:38:22 +0530

    new

--- FOODZONE/settings.py
+++ FOODZONE/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,11 +142,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -155,5 +153,3 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'Food_Zone.Users'
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
-
-# Collect static files
-python3 manage.py collectstatic
+echo "BUILD START..."
+python3 -m pip install -r requirements.txt 
+python3 manage.py collectstatic --noinput --clear

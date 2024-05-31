23ad78ea3bf64d851f2920e6a0f77f51921e37d5
KachhadiyaPrit <kachhadiyaprit89039@gmail.com>
2024-05-31 07:56:59 +0530

    new

--- FOODZONE/urls.py
+++ FOODZONE/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.conf.urls import url
 from Food_Zone import views, admin_views, user_views, vendor_views
 
 urlpatterns = [
@@ -149,7 +150,7 @@ urlpatterns = [
     # Logout
     path('logout', views.logout, name='logout'),
 
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 # ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

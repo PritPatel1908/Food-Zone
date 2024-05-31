69e0409206ef4efdd9562055b129fa829400f205
KachhadiyaPrit <kachhadiyaprit89039@gmail.com>
2024-05-31 07:46:32 +0530

    new

--- FOODZONE/urls.py
+++ FOODZONE/urls.py
@@ -149,6 +149,7 @@ urlpatterns = [
     # Logout
     path('logout', views.logout, name='logout'),
 
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 # ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

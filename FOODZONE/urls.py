7f1644a048d5584fbd21675a7e18b228ad9d35ae
KachhadiyaPrit <kachhadiyaprit89039@gmail.com>
2024-05-31 08:16:23 +0530

    new

--- FOODZONE/urls.py
+++ FOODZONE/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
-from django.conf.urls import url
 from Food_Zone import views, admin_views, user_views, vendor_views
 
 urlpatterns = [

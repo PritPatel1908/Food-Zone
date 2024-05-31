bacb6a30200aa13c3b4995388eb853e1e636a58b
KachhadiyaPrit <kachhadiyaprit89039@gmail.com>
2024-05-31 06:59:06 +0530

    new

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
 echo "BUILD START..."
-python3 -m pip install -r requirements.txt 
+pip install -r requirements.txt 
 python3 manage.py collectstatic --noinput --clear
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput

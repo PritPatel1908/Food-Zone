63a0e4f2bf6115714df4a8849b9560660ccbcea2
KachhadiyaPrit <kachhadiyaprit89039@gmail.com>
2024-05-31 07:02:00 +0530

    new

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo "BUILD START..."
 python3 -m pip install -r requirements.txt 
-python3 -m manage.py collectstatic --noinput --clear
-python3 -m manage.py makemigrations --noinput
-python3 -m manage.py migrate --noinput
+python3 manage.py collectstatic --noinput --clear
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput

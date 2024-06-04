8ddac88288b5d987140deca553b924e4826783ac
KachhadiyaPrit <kachhadiyaprit89039@gmail.com>
2024-06-04 06:48:36 +0530

    new

--- build_files.sh
+++ build_files.sh
@@ -3,5 +3,5 @@ set -o errexit
 python3 -m pip install -r requirements.txt
 
 python3 -m manage.py collectstatic --no-input
-python3 -m manage.py makemigrations
-python3 -m manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate

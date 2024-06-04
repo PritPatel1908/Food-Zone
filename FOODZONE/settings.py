9bf58c0ea35823b40f8715866fdeb6afaff5c830
KachhadiyaPrit <kachhadiyaprit89039@gmail.com>
2024-06-04 07:05:05 +0530

    new

--- FOODZONE/settings.py
+++ FOODZONE/settings.py
@@ -142,14 +142,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'static'
+# STATIC_URL = 'static/'
+# STATIC_ROOT = BASE_DIR/'static'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR,'static')
 # ]
+
+STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

014e1f0ca1a00133e42de4acd569218256decb0c
KachhadiyaPrit <kachhadiyaprit89039@gmail.com>
2024-06-04 06:25:37 +0530

    new

--- FOODZONE/settings.py
+++ FOODZONE/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -144,12 +145,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR/'static'
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR,'static')
 # ]
-
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- build_files.sh
+++
@@ -1,5 +0,0 @@
-echo "BUILD START..."
-python3 pip install -r requirements.txt 
-python3 manage.py collectstatic --noinput --clear
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput

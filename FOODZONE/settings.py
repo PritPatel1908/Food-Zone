572d4a86e551d2f4774749b492a3590e9720106b
KachhadiyaPrit <kachhadiyaprit89039@gmail.com>
2024-06-05 07:26:13 +0530

    new

--- FOODZONE/settings.py
+++ FOODZONE/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'FoodZone',
-        'USER': 'prit',
+        'USER': 'postgres',
         'PASSWORD': 'prit@2004',
         'HOST': 'localhost',
         'PORT': '5432',

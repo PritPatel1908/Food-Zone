f01be02f6c40b1bcffaa5e7956d3db5913ae5e3f
PritPatel1908 <kachhadiyaprit89039@gmail.com>
2024-07-04 12:05:30 +0530

    new

--- FOODZONE/settings.py
+++ FOODZONE/settings.py
@@ -93,9 +93,6 @@ DATABASES = {
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
-        'OPTIONS': {
-            'sql_mode': 'STRICT_ALL_TABLES'
-        }
     }
 }
 

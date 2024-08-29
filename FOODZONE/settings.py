87510f9f58415868cdf53da0c51a469f7ca445b9
PritPatel1908 <kachhadiyaprit89039@gmail.com>
2024-08-29 20:20:33 +0530

    few changes

--- FOODZONE/settings.py
+++ FOODZONE/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'FOODZONE.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'FoodZone',
+        'NAME': 'FoodCom',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
